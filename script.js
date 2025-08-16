5f88f7a34f5a68ba3265d3831a714c114bb85db1
mishka832 <mishkaraheja30@gmail.com>
2025-08-16 23:53:01 +0530

    Made the design responsive and styling better

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
 // let api_key="2f7adeda990e40ffbcdc2bc5c21f177b";
 
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  let input = document.getElementById("input").value;
+  keyword = input;
+  let url = `https://newsdata.io/api/1/latest?apikey=pub_7472446fb022e8ba7430eef102f5b2ab1f2fe&q=${keyword}&country=in`;
 
+  fetch(url)
+    .then((response) => response.json())
+    .then((val) => {
+      console.log(val);
+      let newsContainer = document.getElementById("news-container");
+      newsContainer.innerHTML = "";
+      newsContainer.style.display = "flex";
+      newsContainer.style.flexWrap = "wrap";
+      // newsContainer.style.justifyContent='center';
+      newsContainer.style.alignItems = "flex-start";
+      // newsContainer.style.justifyContent='space-between';
+      newsContainer.style.justifyContent = "space-between";
+      newsContainer.style.gap = "5px";
+      newsContainer.style.background =
+        "linear-gradient(135deg, #0f0f1a, #1a1a2e, #241b38);";
+      newsContainer.style.borderRadius = "12px";
 
-button.addEventListener('click',function(e){
-    e.preventDefault();
-    let input = document.getElementById("input").value;
-    keyword = input;
-    let url = `https://newsdata.io/api/1/latest?apikey=pub_7472446fb022e8ba7430eef102f5b2ab1f2fe&q=${keyword}&country=in`;
-    
-    fetch(url)
-   .then(response => response.json()).then((val)=>{
-    console.log(val);
-    let newsContainer=document.getElementById('news-container');
-    newsContainer.innerHTML='';
-    newsContainer.style.display='flex';
-    newsContainer.style.flexWrap='wrap';
-    // newsContainer.style.justifyContent='center';
-    newsContainer.style.alignItems='flex-start';
-    // newsContainer.style.justifyContent='space-between';
-    newsContainer.style.justifyContent='space-between';
-    newsContainer.style.gap='5px';
-    newsContainer.style.background='linear-gradient(to bottom, #E0F7FA, #B2EBF2)';
+      newsContainer.style.gap = "10px";
+      for (let i = 0; i < val.results.length; i++) {
+        for (let i = 0; i < val.results.length; i++) {
+          let newsArticle = document.createElement("div");
+          newsArticle.classList.add("news-article");
 
+          let description = val.results[i].description
+            ? val.results[i].description.length > 100
+              ? val.results[i].description.substring(0, 500) + "..."
+              : val.results[i].description
+            : "No Description available.";
 
-    newsContainer.style.gap='10px';
-    for(let i=0;i<val.results.length;i++) {
-        let newsArticle=document.createElement('div');
-        newsArticle.style.background = '#FFF8E3';
-        newsArticle.style.width = '400px';
-        newsArticle.style.padding = '15px';
-        newsArticle.style.color = 'white';
-        newsArticle.style.borderRadius = '8px';
+          newsArticle.innerHTML = `
+    <h2 class="news-title">${val.results[i].title}</h2>
+    <p class="news-desc">${description}</p>
+    <a class="news-link" href="${val.results[i].link}" target="_blank">Read More</a>
+  `;
 
-        newsArticle.style.overflow = 'hidden';
-        let description = val.results[i].description
-                    ? val.results[i].description.length > 100
-                        ? val.results[i].description.substring(0, 500) + "..."
-                        : val.results[i].description
-                    : "No Description available.";
-        newsArticle.innerHTML=`
-        <h2 style="color:#0F4C5C; font-weight:bold">${val.results[i].title}</h2>
-        <p style="color:#333333;">${description}</p>
-        <a href="${val.results[i].link}" target="_blank">Read More</a>
-        `;
-        
-        newsContainer.appendChild(newsArticle);
-        
-    }
-   })
-})
-
-
+          newsContainer.appendChild(newsArticle);
+        }
+      }
+    });
+});
 
 // let newsForm = document.getElementById("newsForm");
 // let button=document.getElementById("button");
@@ -62,15 +57,12 @@ button.addEventListener('click',function(e){
 //     fetch(url)
 //     .then(response => response.json())
 //     .then(data => {
-        
+
 //         console.log("Search Results:", data);
 //         let newsContainer = document.getElementById("newsContainer");
 //         console.log(data.news[0].title)
-        
+
 //     })
 //     .catch(error => console.error("Error fetching news:", error));
 
-
-
 // })
-
